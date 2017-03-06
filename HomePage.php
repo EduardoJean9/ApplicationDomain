bb5d8fb75e43a9836aca31b5b4dbe317c714cd12
Eduardo Jean <eduardojean9@gmail.com>
2017-03-06 17:04:21 -0500

    Added Event log

--- HomePage.php
+++ HomePage.php
@@ -50,13 +50,7 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="HomePage.php">Application Domain</a>
+                <a class="navbar-brand" href="HomePage.php">Black Bird Accounting</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <?php
@@ -141,7 +135,7 @@
                     echo "<button id=\"backButton\" class=\"btn btn-primary\" onclick=\"history.go(-1);\">Back </button>";
                 }
             ?>
-            
+
         </div>
         <!-- /.row -->
 
